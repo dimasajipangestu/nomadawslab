c254676b8f320ae1306444422a2a6ad6a0d8e786
dimasajipangestu <dimasajipangestu@gmail.com>
2021-08-03 21:42:31 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,3 @@ output "consul_url" {
 output "nomad_url" {
   	value = "http://${aws_instance.nomad-node[0].public_ip}:4646/ui"
 }
-
-output "ec2_nodes" {
-	value = aws_instance.nomad-node[*].public_dns
-}
-
-output "connection_info" {
-	value = "ssh -i ${aws_instance.nomad-node[*].key_name} ${aws_instance.nomad-node[*].public_dns}"
-}
